3e1c6b832cafee77adcfcd7cd1fe41c0769b2820
meiyaismywaifu <51034737+meiyaismywaifu@users.noreply.github.com>
2020-09-27 22:58:37 -0700

    recursion fixes

--- recursion/fibonacci.rb
+++ recursion/fibonacci.rb
@@ -9,7 +9,6 @@ def fibs(n)
    return carrier
 end
 
-
 def fib(n)
    return [0]   if n == 0
    return [0,1] if n == 1
--- recursion/merge_sort.rb
+++ recursion/merge_sort.rb
@@ -4,9 +4,9 @@ def merge_sort(array)
    else
       middle = array.length/2.floor-1
       left = merge_sort(array[0..middle])
-      right = merge_sort(array[middle..(array.length-1)])
+      right = merge_sort(array[middle+1..-1])
+      
       sorted = []
-
       until left.length == 0 && right.length == 0
          case
          when right.length == 0
@@ -19,7 +19,6 @@ def merge_sort(array)
             sorted << right.slice!(0)
          end
       end
-      
       return sorted
    end
 end
